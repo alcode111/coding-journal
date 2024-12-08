941ad7898bd7db939f7eeb1291c2aeb6ac20b87f
alcode111 <al.code.111@gmail.com>
2024-12-08 21:13:43 +0100

    Add xcode project files

---
+++ CodingJournal/CodingJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingJournalApp.swift
+//  CodingJournal
+//
+//  Created by Ismaïl on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct CodingJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
