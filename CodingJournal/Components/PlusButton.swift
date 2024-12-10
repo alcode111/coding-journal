b580e64a4c92231663bd61b7838bb75aef9f07e9
alcode111 <al.code.111@gmail.com>
2024-12-10 15:40:23 +0100

    Add PlusButton component

---
+++ CodingJournal/Components/PlusButton.swift
@@ -0,0 +1,18 @@
+//
+//  PlusButton.swift
+//  CodingJournal
+//
+//  Created by Ismail Larbi Pacha on 10/12/24.
+//
+
+import SwiftUI
+
+struct PlusButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlusButton()
+}
