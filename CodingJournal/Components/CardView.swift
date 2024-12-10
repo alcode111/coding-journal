9765b19d1759f45f6540f0b82fd3dac35c19f2d8
alcode111 <al.code.111@gmail.com>
2024-12-10 15:38:45 +0100

    Add component folder, create Card View

---
+++ CodingJournal/Components/CardView.swift
@@ -0,0 +1,63 @@
+//
+//  CardView.swift
+//  CodingJournal
+//
+//  Created by Ismail Larbi Pacha on 10/12/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading) {
+                    Text("The 1st rule of poetry club")
+                        .font(.title3.bold())
+                    
+                    Text("GitHub Classroom")
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                }
+                .padding(.top, 25)
+                .padding(.horizontal, 25)
+                .padding(.bottom, 8)
+                
+                Spacer().frame(height: 25)
+                
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 2)
+                    .foregroundStyle(.white)
+                
+                HStack {
+                    Text("Fri, 6th Dec")
+                        .font(.caption)
+                    
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("...")
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                }
+                .padding(.horizontal, 25)
+                .padding(.bottom, 8)
+            }
+        }
+        .background(.cardBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 28))
+        .overlay(
+            RoundedRectangle(cornerRadius: 28)
+                .stroke(.white, lineWidth: 2.5)
+            )
+        .padding(2)
+    }
+}
+
+#Preview {
+    CardView()
+}
