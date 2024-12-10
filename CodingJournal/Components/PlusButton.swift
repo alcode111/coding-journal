c660a9d9a611b5f68c857f777e9391f3eab0bc00
alcode111 <al.code.111@gmail.com>
2024-12-10 16:26:09 +0100

    Remove unnecessary spaces

--- CodingJournal/Components/PlusButton.swift
+++ CodingJournal/Components/PlusButton.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct PlusButton: View {
+    @Binding var isShowingModal: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            isShowingModal.toggle()
+        } label: {
+            ZStack {
+                Circle()
+                    .fill(.mainBackground)
+                    .frame(width: 70, height: 70)
+                    .overlay {
+                        Circle()
+                            .stroke(.white, lineWidth: 2)
+                    }
+                
+                Image(systemName: "plus")
+                    .font(.title.bold())
+                    .foregroundStyle(.white)
+            }
+        }
     }
 }
 
 #Preview {
-    PlusButton()
+    PlusButton(isShowingModal: .constant(false))
 }
